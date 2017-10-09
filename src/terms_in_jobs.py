fb383a6dd1880396d44ada88b58e04de58fa2051
Glenn Hintze <glenn@glennhintze.com>
2017-10-09 16:50:13 -0500

    filename changed to reflect purpose. added bulk processing.

--- src/terms_in_jobs.py
+++ src/terms_in_jobs.py
@@ -1,42 +1,54 @@
-searchfile = open("/Users/glennhintze/data/Data Scientist.txt", "r")
-termfile = open("/Users/glennhintze/data/keywords.txt", "r")
+import os
 
-# variables
-termlist = []
-resultlist = []
-totalcount = 0
-
-countcheck = 0 #debug
-
-
-# build the list of terms to search for
-for line in termfile:
-    term = str(line).replace("\r\n","")
-    termlist.append(term)
-    
-# search for each term in each line and append to results if found
-for line in searchfile:
-    for term in termlist: 
-        if term in line: 
-            totalcount += 1
-            resultlist.append(term)
-              
-# aggregate the results
-for term in termlist:
-    termcount = 0
-    for result in resultlist:
-        if term == result:
-            termcount += 1
-    countcheck += termcount #debug
-    print term, ":", termcount
+filelist = []
+rootdir = '/Users/glennhintze/data/jobs'
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        print os.path.join(subdir, file)
+        filelist.append(os.path.join(subdir, file))
         
-        
-        
-        
-searchfile.close()
-termfile.close()
+print filelist #debug
+
+for file in filelist:
+    searchfile = open(file)
+    termfile = open("/Users/glennhintze/data/keywords.txt", "r")
+    outfile = open("/Users/glennhintze/data/output.txt", "w")
+
+    # variables
+    termlist = []
+    resultlist = []
+    totalcount = 0
+
+    countcheck = 0 #debug
 
 
-#print resultlist
-print "total count: ", totalcount
-print "sum of counts: ", countcheck
+    # build the list of terms to search for
+    for line in termfile:
+        term = str(line).replace("\r\n","")
+        termlist.append(term)
+
+    # search for each term in each line and append to results if found
+    for line in searchfile:
+        for term in termlist: 
+            if term in line: 
+                totalcount += 1
+                resultlist.append(term)
+
+    # aggregate the results
+    for term in termlist:
+        termcount = 0
+        for result in resultlist:
+            if term == result:
+                termcount += 1
+        countcheck += termcount #debug
+        print term, ":", termcount
+        outfile.write("%s : %d\n" % (term, termcount))
+
+
+    # close the files
+    searchfile.close()
+    termfile.close()
+
+    # print debug check
+    print "total count: ", totalcount
+    print "sum of counts: ", countcheck
