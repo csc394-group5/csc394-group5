3cc1dda5f228ffb9d2cb83ac2b8eb8b39a06ea84
Glenn Hintze <glenn@glennhintze.com>
2017-10-13 16:08:40 -0500

    created sql script to create and populate Degrees table

---
+++ src/degrees_script.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Degrees (
+    DegreeTitle varchar(5),
+    DegreeName varchar(255),
+    DegreeConcentration varchar(255),
+    DegreeSchool varchar(255),
+);
+
+INSERT INTO Degrees VALUES ("MA", "Animation", "Animator", "Cinematic Arts");
+INSERT INTO Degrees VALUES ("MA", "Animation", "Technical Artist", "Cinematic Arts");
+INSERT INTO Degrees VALUES ("MS", "Applied Technology", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Business Information Technology", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Computational Finance", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Computer Science", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Cybersecurity", "Computer Security", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Cybersecurity", "Governance, Risk Management and Compliance", "Computing");
+INSERT INTO Degrees VALUES ("MA", "Digital Communication and Media Arts", "", "Design");
+INSERT INTO Degrees VALUES ("MS", "E-Commerce Technology", "", "Computing");
+INSERT INTO Degrees VALUES ("MA", "Experience Design", "", "Design");
+INSERT INTO Degrees VALUES ("MS", "Film and Television", "Cinematography", "Cinematic Arts");
+INSERT INTO Degrees VALUES ("MS", "Film and Television", "Post-Production", "Cinematic Arts");
+INSERT INTO Degrees VALUES ("MS", "Film and Television", "Sound", "Cinematic Arts");
+INSERT INTO Degrees VALUES ("MS", "Game Programming", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Health Informatics", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Human-Computer Interaction", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Information Systems", "Business Analysis/Systems Analysis", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Information Systems", "Business Intelligence", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Information Systems", "Database Administration", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Information Systems", "IT Enterprise Management", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Information Systems", "Standard", "Computing");
+INSERT INTO Degrees VALUES ("MS", "IT Project Management", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Network Engineering and Security", "", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Predictive Analytics", "Computational Methods", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Predictive Analytics", "Health Care", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Predictive Analytics", "Hospitality", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Predictive Analytics", "Marketing", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Software Engineering", "Entrepreneurship & Technology Leadership", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Software Engineering", "Real-Time Game Systems", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Software Engineering", "Software Architecture", "Computing");
+INSERT INTO Degrees VALUES ("MS", "Software Engineering", "Software Development", "Computing");
+INSERT INTO Degrees VALUES ("JD/MS", "Computer Science Technology", "", "Computing");
