4fa62968e34ac1e45aa609bc683179e55737c392
Glenn Hintze <glenn@glennhintze.com>
2017-10-13 16:21:29 -0500

    created sql script to create Classes table

---
+++ src/classes_script.sql
@@ -0,0 +1,164 @@
+CREATE TABLE AnimationAnimator (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE AnimationTechnicalArtist (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE AppliedTechnology (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE BusinessInformationTechnology (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ComputationalFinance (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ComputerScience (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE CybersecurityComputerSecurity (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE CybersecurityGovernanceRiskManagementCompliance (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE DigitalCommunicationMediaArts (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ECommerceTechnology (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ExperienceDesign (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE FilmTelevisionCinematography (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE FilmTelevisionPostProduction (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE FilmTelevisionSound (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE GameProgramming (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE HealthInformatics (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE HumanComputerInteraction (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE InformationSystemsBusinessAnalysisSystemsAnalysis (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE InformationSystemsBusinessIntelligence (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE InformationSystemsDatabaseAdministration (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE InformationSystemsITEnterpriseManagement (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE InformationSystemsStandard (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ITProjectManagement (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE NetworkEngineeringSecurity (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE PredictiveAnalyticsComputationalMethods (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE PredictiveAnalyticsHealthCare (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE PredictiveAnalyticsHospitality (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE PredictiveAnalyticsMarketing (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE SoftwareEngineeringEntrepreneurshipTechnologyLeadership (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE SoftwareEngineeringRealTimeGameSystems (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE SoftwareEngineeringSoftwareArchitecture (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE SoftwareEngineeringSoftwareDevelopment (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
+
+CREATE TABLE ComputerScienceTechnology (
+    ClassNumber varchar(10),
+    ClassName varchar(255),
+);
